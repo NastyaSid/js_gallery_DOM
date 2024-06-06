8c48ea398bebbe795b8bd642101fe7d411340e4c
Nastya <anastasiia.y.sitnikova@gmail.com>
2024-06-06 17:22:49 +0200

    make build

---
+++ index.de08d943.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item");if(t)return document.querySelector(".gallery__large-img").src=t.querySelector(".list-item__link").href});
+//# sourceMappingURL=index.de08d943.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const thumbNail = e.target.closest(".list-item");
+    if (!thumbNail) return;
+    const img = document.querySelector(".gallery__large-img");
+    return img.src = thumbNail.querySelector(".list-item__link").href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
